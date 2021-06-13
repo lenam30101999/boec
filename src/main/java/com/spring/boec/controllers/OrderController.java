6a6194f3436cb2632fa11ea380557153eddb2900
phamphilongPTIT <phamphilong4101999@gmail.com>
2021-06-13 12:08:34 +0700

    add function update state order

--- src/main/java/com/spring/boec/controllers/OrderController.java
+++ src/main/java/com/spring/boec/controllers/OrderController.java
@@ -9,10 +9,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
 
@@ -28,6 +25,15 @@ public class OrderController {
         OrderDTO orderDTO = orderService.getOrderByCustomer(customerId);
         if (Objects.nonNull(orderDTO)){
             return new ResponseEntity<>(orderDTO, HttpStatus.OK);
+        }else
+            return new ResponseEntity<>(new MessageDTO(Util.CART_IS_EMPTY), HttpStatus.BAD_REQUEST);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateState(@RequestBody OrderDTO orderDTO){
+        OrderDTO orderDTO1 = orderService.updateState(orderDTO);
+        if (Objects.nonNull(orderDTO1)){
+            return new ResponseEntity<>(new MessageDTO(Util.UPDATED_SUCCESS), HttpStatus.OK);
         }else
             return new ResponseEntity<>(new MessageDTO(Util.CART_IS_EMPTY), HttpStatus.BAD_REQUEST);
     }
--- src/main/java/com/spring/boec/services/OrderService.java
+++ src/main/java/com/spring/boec/services/OrderService.java
@@ -6,6 +6,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Objects;
 
 @Log4j2
 @Service
@@ -15,6 +16,16 @@ public class OrderService extends BaseService {
     public OrderDTO getOrderByCustomer(int customerId){
         Order order = orderRepository.findTopByCustomerIdOrderByIdDesc(customerId);
         if (order != null && !order.getPayment().isPaid()){
+            return modelMapper.convertOrderDTO(order);
+        }
+        return null;
+    }
+
+    public OrderDTO updateState(OrderDTO orderDTO){
+        Order order = orderRepository.findById(orderDTO.getId());
+        if (Objects.nonNull(order)){
+            order.setState(orderDTO.getState());
+            orderRepository.saveAndFlush(order);
             return modelMapper.convertOrderDTO(order);
         }
         return null;
